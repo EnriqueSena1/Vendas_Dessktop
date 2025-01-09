b2ea7f8242184196a94e94a032bb81c145a08a42
EnriqueSena1 <enriquesenacontato07@gmail.com>
2025-01-08 22:29:34 -0300

    fix: Obrigatoriedade.
    criamos o método mudarTexto() adicionamos dois parâmetros: o self e o event. Nesse caso eles são obrigatórios para o funcionamento e devem ser sempre os dois primeiros parâmetros do método.
    
    Outra opção que temos é passar diretamente o comando como atributo do widget

--- paginaLogin/index.py
+++ paginaLogin/index.py
@@ -6,14 +6,20 @@ class Application:
         self.widget1 = Frame(master)
         self.widget1.pack()
         self.msg = Label(self.widget1, text="Primeiro Widget")
-        self.msg["font"] = ("Verdana", "10", "italic","bold") # tive que colocar o Italic com letra minuscula rsrs-> resolução (italic)
+        self.msg["font"] = ("Calibri", "9", "italic") # tive que colocar o Italic com letra minuscula rsrs-> resolução (italic)
         self.msg.pack () 
         self.sair = Button(self.widget1)
-        self.sair["text"] = "Sair"
-        self.sair["font"] = ("Verdana", "10")
-        self.sair["width"] = 5
-        self.sair["command"] = self.widget1.quit
-        self.sair.pack (side=RIGHT)
+        self.sair["text"] = "Clique aqui"
+        self.sair["font"] = ("Calibri", "9")
+        self.sair["width"] = 10
+        self.sair.bind("<Button-1>", self.mudarTexto)
+        self.sair.pack ()
+
+    def mudarTexto(self, event):
+        if self.msg["text"] == "Priemiro Widget":
+            self.msg["text"] = "O Botão Receu um Clique"
+        else:
+            self.msg["text"] = "Primeiro Widget"
 
 root = Tk()
 Application(root)
